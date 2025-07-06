2f11f8cdf8815dc8b256de533f94ae41fa4c427f
gks-code <gksingh1106@gmail.com>
2025-07-06 18:55:50 +0000

    problem set 0

---
+++ ps0/indoor.py
@@ -0,0 +1,7 @@
+#ask user for input and then change it to all lowercase (.lower is used to change every alphabets to lowercase)
+
+userinput = input ("type anything. ").lower()
+
+
+#after changing return it to user 
+print (userinput)
