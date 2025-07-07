d4c9542dfd4dac123a396b16451b42651781a427
Gaurav kumar singh <gksingh1106@gmail.com>
2025-07-07 19:31:58 +0530

    Update faces.py

--- ps0/faces.py
+++ ps0/faces.py
@@ -1,25 +1,14 @@
-#defined function convert() and gave an argument 'face' whicn is variable 
-def convert(face):
-
-#also replaced value to emoji if variable face have :) or :(
-    face = face.replace (":)" , "🙂")
-    face = face.replace (":(" , "☹️")
-
-
-#return the input value to user after making desired change 
-    return face 
-
-#input is stored in variable face after taking it from user
 def main():
-    face = input ("what's your name? ")
+    text = input()
+    print(convert(text))
 
- #print function convert which has input Variable face as argument    
-    print(convert(face))
+def convert(text):
+    text = text.replace(":)", "🙂")
+    text = text.replace(":(", "🙁")
+    return text
 
+main()
 
-
-if __name__ == "__main__":
-    main()
 
 
 
