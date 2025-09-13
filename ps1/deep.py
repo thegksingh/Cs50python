19795cc9c88e99e3ed8f28889f77fb9033e344be
gks-code <gksingh1106@gmail.com>
2025-09-13 04:37:58 +0000

    ps1 update

--- ps1/deep.py
+++ ps1/deep.py
@@ -1,4 +1,4 @@
-universe = input("what is the Answer to the Great Question of Life, the Universe, and Everything? ")
+universe = input("what is the Answer to the Great Question of Life, the Universe, and Everything? ").strip().lower()
 if universe == "42" or universe =="forty-two" or  universe =="forty two":
      print("Yes")
 else:
