3755b57b4d2a8e7f9c69a28551da1fc02949bf7c
Gaurav kumar singh <gksingh1106@gmail.com>
2025-09-19 14:48:28 +0530

    Update figlet.py

--- ps4/figlet.py
+++ ps4/figlet.py
@@ -3,19 +3,22 @@ import sys
 import random
 
 
-f = pyfiglet.Figlet()
+figlet = pyfiglet.Figlet()
 if len(sys.argv) == 3:
-    if sys.argv[1] in ["-f", "--font"]:
-        text = input("Input: ").strip()
-        if sys.argv[2] not in f.getFonts():
-            sys.exit("Invalid usage")
-        f.setFont(font=sys.argv[2])
-        print(f.renderText(text))
+    if sys.argv[1] != "-f" and sys.argv[1] != "--font":
+        sys.exit("Invalid usage")
+    if sys.argv[2] not in figlet.getFonts():
+        sys.exit("Invalid usage")
+    text = input("Input: ")
+    figlet.setFont(font=sys.argv[2])
+    print(figlet.renderText(text))
 
 elif len(sys.argv) == 1:
     text = input("Input: ").strip()
-    f.setFont = random.choice(f.getFonts())
-    print(f.renderText(text))
+    font = random.choice(figlet.getFonts())
+    figlet.setFont(font = font)
+    print(figlet.renderText(text))
 
 else:
     sys.exit("Invalid usage")
+
