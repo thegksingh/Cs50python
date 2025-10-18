5356228f343cbacd221fabd0fb43c926e2723b3f
thegksingh <gksingh1106@gmai.com>
2025-10-18 09:31:49 +0530

    solved watch.py of ps7

---
+++ ps7/watch.py
@@ -0,0 +1,18 @@
+import re
+import sys
+
+def main():
+    print(prase(input("HTML: ")))
+
+
+def prase(s):
+    match = re.search(r'src="https?://(?:www\.)?youtube\.com/embed/([a-zA-Z0-9_-]+)"', s)
+    if match:
+        iframe = match.group(1)
+        return f"https://youtu.be/{iframe}"
+    return None
+
+
+
+if __name__ == "__main__":
+    main()      
